f1c4cddd9bd835cf8c5bc17a7329af66ce5223ac
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-01 20:42:12 -0500

    A function to check if a node is a leaf-node

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,7 +11,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->right == NULL && node->left == NULL)
-		return (1);
+	if (node->right == NULL && node->left == NULL)i 
+		return (1); /*A leaf node has no children*/
 	return (0);
 }
