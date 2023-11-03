ca99c3ba749268cc8f768ce41636431cf45df62d
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-02 21:18:16 -0500

    Function that  counts the nodes with at least 1 child in a binary tree and function that checks if a binary tree is full

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,13 +6,16 @@
  * Return: the number of nodes
  * if tree is null, return 0
 */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
+
 	if (tree->right == NULL && tree->left == NULL)
 		return (0);
+
 
 	return (1 + binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left));
 }
